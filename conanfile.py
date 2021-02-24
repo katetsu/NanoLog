82622065337ea729dc8725d9910272088e8cc2e0
Hoss Ajallooiean <hossein.ajallooiean@zhinst.com>
2021-02-24 14:14:56 +0100

    f

---
+++ conanfile.py
@@ -0,0 +1,36 @@
+from conans import ConanFile, CMake, tools
+
+
+class NanoLogConan(ConanFile):
+    name = "nanolog"
+    version = "0.91"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Hello here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/PlatformLab/NanoLog.git")
+        self.run("cd nanolog")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="nanolog")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="nanolog")
+        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["nanolog"]
+
